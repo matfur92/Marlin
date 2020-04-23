a36dbd68ae9a6f7e13c8f7fdcf86d6bacb663762
Matteo Furlan <mat_fur_92_pub@yahoo.it>
2020-04-24 00:53:22 +0200

    Working and tested A4988 version

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "MendelMaxMF"
+#define CUSTOM_MACHINE_NAME "MendelMaxMF" //A4988
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
